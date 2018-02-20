5f995d25e9e487206ee358a473feab59083e697d
Damian Romanów <damian.romanow@gmail.com>
2018-02-20 22:59:53 +0100

    code cleanup

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
 'use strict';
 
-const Filter  = require('broccoli-filter')
-const fs      = require('fs')
-const yaml    = require('js-yaml')
-const _       = require('lodash/fp')
-const jf      = require('jsonfile')
-const fse     = require('fs-extra')
-
-class I18nFilter extends Filter {
-  constructor(inputNode, options) {
-    super(inputNode, options)
-  }
-
-  getDestFilePath(path) {
-    if(/.*\/translations\..*\.(yaml|yml)/.test(path))
-      return path
-  }
-
-  processString(source, relativePath) {
-    let locale = /translations\.(.*)\..*/.exec(relativePath)[1]
-    let content = yaml.safeLoad(source)
-    let key = relativePath.split('/').slice(2).reverse().slice(1).reverse().join('.')
-    let localeFilePath = `app/locales/${locale}/i18n-pod.js`
-
-
-    let finalize = (translations) => {
-      let json = JSON.stringify(_.set(key, content, translations), null, 2)
-      fse.outputFileSync(localeFilePath, `export default ${json}`)
-    }
-
-    if(fs.existsSync(localeFilePath)) {
-      fs.readFile(localeFilePath, "utf8", function (error, content) {
-        finalize(JSON.parse(content.replace('export default', '')))
-      })
-    } else {
-      finalize({})
-    }
-  }
-}
+const utils      = require('./lib/utils')
+const commands   = require('./lib/commands')
+const I18nFilter = require('./lib/i18n-filter')
 
 module.exports = {
   name: 'ember-i18n-pod',
@@ -46,11 +11,15 @@ module.exports = {
     return true
   },
 
+  includedCommands() {
+    return commands
+  },
+
   setupPreprocessorRegistry(type, registry) {
     if (type === 'parent') {
       registry.add('js', {
         name: 'ember-i18n-pod',
-        ext: 'yml',
+        ext: ['json', 'yml'],
         toTree(tree) {
           return new I18nFilter(tree)
         }
